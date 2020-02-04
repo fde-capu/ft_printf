4d220cd482286461487f6f28ce624adfad3d5b83
flacarrara <flacarrara@gmail.com>
2020-02-03 22:04:43 -0300

    going

--- ft_printf.c
+++ ft_printf.c
@@ -6,17 +6,14 @@
 /*   By: fde-capu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/03 12:47:29 by fde-capu          #+#    #+#             */
-/* U20200203210153 ||:|:|                      */
+/* U20200203220244 :|:|||                      */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdio.h> // REM
 
 void	pexe(va_list a, void *x)
 {
-	if (ft_strcmp(x, "int"))
-		printf(" test >>> int >>> %d\n", *(int *)x); // REM
 	return ;
 }
 
@@ -27,7 +24,8 @@ int		ft_printf(const char *fmt, ...)
 	va_start(a, (char *)fmt);
 	while (*fmt)
 	{
-		pexe(a, "int");
+		//pexe(a, "int");
+		ft_putchar_fd(*fmt, FDOUT);
 		fmt++;
 	}
 	va_end(a);
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: fde-capu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/03 12:49:13 by fde-capu          #+#    #+#             */
-/* U20200203210405 |:|:::                      */
+/* U20200203220154 ::||:|                      */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 # include <stdarg.h>
 # include "libft/libft.h"
 # include <stdio.h> // REM
+# define FDOUT 1
 
 int		ft_printf(const char *a, ...);
 void	pexe(va_list a, void *x);
