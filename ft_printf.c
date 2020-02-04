e2be8680c40321c50c5ae99e383a5de86149d716
flacarrara <flacarrara@gmail.com>
2020-02-03 23:15:14 -0300

    new method

--- ft_printf.c
+++ ft_printf.c
@@ -6,46 +6,49 @@
 /*   By: fde-capu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/03 12:47:29 by fde-capu          #+#    #+#             */
-/* U20200203223847 ::|:|:                      */
+/* U20200203231505 :::||:                      */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static void	pexe(va_list a, void *x)
+static int	joker(char **str)
 {
-	return ;
-}
-
-static char	*joker(char *str)
-{
-	if (!ft_strncmp(str, "%c", 2))
+	if (!ft_strncmp(*str, "%c", 2))
 	{
-		return (str + 2 - 1);
+		*str += 2;
+		return (1);
 	}
-	return (str);
+	return (0);
 }
 
 int			ft_printf(const char *fmt, ...)
 {
 	va_list	a;
-	char	*f;
+	char	**f;
+	char	*s;
 	char	*t;
+	int		type;
 
-	f = (char *)fmt;
+	s = (char *)fmt;
+	f = &s;
 	va_start(a, fmt);
-	while (*f)
+	while (*s)
 	{
-		//pexe(a, "int");
-		if (f == joker(f))
-			ft_putchar_fd(*f, FDOUT);
+		type = joker(f);
+		if (!type)
+		{
+			ft_putchar_fd(*s, FDOUT);
+			s++;
+		}
 		else
 		{
-			f = joker(f);
-			t = va_arg(a, char *);
-			ft_putstr_fd(t, FDOUT);
+			if (type == 1)
+			{
+				t = va_arg(a, char *);
+				ft_putstr_fd(t, FDOUT);
+			}
 		}
-		f++;
 	}
 	va_end(a);
 	return (1);
